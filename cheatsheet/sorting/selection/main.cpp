4f656b497c0994f2446e20daf15238c7a8bb32bb
Khittitaj BUNUPURADAH <khittitaj.b@ku,th>
2025-10-09 17:40:39 +0700

    mustarddddddddd

---
+++ cheatsheet/sorting/selection/main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void selectionSort(vector<int>& arr) {
+    int n = arr.size();
+    for (int i = 0; i < n - 1; i++) {
+        int minIdx = i;
+        for (int j = i + 1; j < n; j++)
+            if (arr[j] < arr[minIdx]) minIdx = j;
+        swap(arr[i], arr[minIdx]);
+    }
+}
